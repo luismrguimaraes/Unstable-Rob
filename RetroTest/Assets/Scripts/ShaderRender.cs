415179bd56340c4ba50a2245faec092558736812
Francisco Cerqueira <up201905337@fe.up.pt>
2024-03-02 14:38:27 +0000

    feat: add glitching interpolation

--- RetroTest/Assets/Scripts/ShaderRender.cs
+++ RetroTest/Assets/Scripts/ShaderRender.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Kino;
 using UnityEngine;
 
 public class ShaderRender : MonoBehaviour
@@ -8,16 +9,27 @@ public class ShaderRender : MonoBehaviour
     public Gradient gradient;
     public HeatControl heatControl;
     
+    public DigitalGlitch digitalGlitch;
+    public AnalogGlitch analogGlitch;
+    
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         Graphics.Blit(source, destination, material);
     }
 
    private void Update()
-    {
-        float level = heatControl.HeatLevel;
+   {
+       float level = heatControl.HeatLevel;
         Color matColor = gradient.Evaluate(level);
         material.SetColor("_Color", matColor);
+        
+        level = Mathf.Pow(heatControl.HeatLevel, 3);
+        digitalGlitch.intensity = Mathf.Lerp(0, 0.11f, level);
+        
+        level = Mathf.Pow(heatControl.HeatLevel, 2);
+        analogGlitch.scanLineJitter = Mathf.Lerp(0, 0.5f, level);
+        analogGlitch.verticalJump = Mathf.Lerp(0, 0.155f, level);
+        analogGlitch.horizontalShake = Mathf.Lerp(0, 0.04f, level);
     }
 
 }
