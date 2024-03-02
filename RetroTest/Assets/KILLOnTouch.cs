f82fd1deb8dc3587d9973f1ddb47b372b4701373
Dudugs <duartesardao@sapo.pt>
2024-03-02 15:48:03 +0000

    killer more level 2 stuff

---
+++ RetroTest/Assets/KILLOnTouch.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KILLOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Destroy(collision.gameObject); //dont do this, call a death function
+    }
+}
